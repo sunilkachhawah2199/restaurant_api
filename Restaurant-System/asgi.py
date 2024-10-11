7849516cdb150dd7de1ab7c068b7503bda29761c
Sunil Kachhawah <sunilkachhawah0521@gmail.com>
2024-10-12 00:08:35 +0530

    naming convention changed

--- Restaurant-System/asgi.py
+++ Restaurant-System/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for learningProject project.
+ASGI config for Restaurant-System project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learningProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurant-System.settings')
 
 application = get_asgi_application()
--- Restaurant-System/wsgi.py
+++ Restaurant-System/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for learningProject project.
+WSGI config for Restaurant-System project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learningProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurant-System.settings')
 
 application = get_wsgi_application()
